789ea241a33ad97bde442a5ac871d810dd6247b4
jvilla1991 <villajeremy1991@gmail.com>
2017-04-13 10:21:44 -0400

    initial commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int printUserNumber(int x)
+    {
+        return x;
+    }
+
+    public static void main(String[] args)
+    {
+        long i = 0;
+        long userInt = 0;
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.print("Please enter an integer: ");
+        userInt = scnr.nextInt();
+
+        System.out.println("Number   " + "   Squared   " + "   Cubed");
+        for (i = 1; i <= userInt; ++i)
+        {
+            System.out.println((i) + "           " + (i * i) + "            " + (i * i * i));
+        }
+        
+
+
+
+    }
+}
